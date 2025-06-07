2d17dffc6dc61e3eaf584b38aaf2449eee1bf9ec
nasrul1221 <fst20071221@gmail.com>
2025-06-07 20:05:22 +0300

    Obtain data for charts via json files

--- src/components/pages/CreatePage.jsx
+++ src/components/pages/CreatePage.jsx
@@ -1,36 +1,60 @@
 import Chart from "react-apexcharts";
+import { useState } from "react";
 
 export default function CreatePage() {
-  const options = {
-    chart: {
-      id: "basic-bar",
-    },
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-    },
-  };
+  const [fileName, setFileName] = useState("");
+  const [data, setData] = useState(null);
 
-  const series = [
-    {
-      name: "series-1",
-      data: [30, 40, 45, 50, 49, 60, 70, 91],
-    },
-  ];
+  const handleFile = (e) => {
+    setFileName(e.target.files[0]?.name);
+    const file = e.target.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      try {
+        const json = JSON.parse(event.target.result);
+        setData(json);
+        console.log(json);
+      } catch (err) {
+        alert("Error while reading JSON: " + err.message);
+      }
+    };
+
+    reader.readAsText(file);
+  };
 
   return (
     <div className="flex flex-1 justify-between p-10 ">
-      <form className="rounded border-black shadow-lg p-8">
-        <label htmlFor="chart-type">Select a type of a chart: </label>
-        <select name="chart-type" id="chart-type">
-          <option value={"line"}>Line</option>
-          <option value={"area"}>Area</option>
-          <option value={"column"}>Column</option>
-          <option value={"boxPlot"}>BoxPlot</option>
-        </select>
-      </form>
+      <div>
+        <label
+          htmlFor="fileUpload"
+          className="cursor-pointer px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+        >
+          Вибрати файл
+        </label>
+
+        <input
+          id="fileUpload"
+          type="file"
+          className="hidden"
+          onChange={handleFile}
+        />
+
+        <span className="text-sm text-gray-700">{fileName}</span>
+      </div>
 
       <div>
-        <Chart options={options} series={series} type="bar" width="500" />
+        {data && (
+          <Chart
+            options={data.options}
+            series={data.series}
+            height={data.height}
+            width={data.width}
+          />
+        )}
       </div>
     </div>
   );
