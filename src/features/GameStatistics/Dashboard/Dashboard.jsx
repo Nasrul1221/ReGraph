4b68985f4e06cff3724da0f8640af0d25429ccb3
nasrul1221 <fst20071221@gmail.com>
2025-06-26 21:58:57 +0300

    Refactor Dashboard component; enhance pie chart data handling and state management; update chart options for better representation

--- src/features/GameStatistics/Dashboard/Dashboard.jsx
+++ src/features/GameStatistics/Dashboard/Dashboard.jsx
@@ -4,24 +4,36 @@ import * as chartTemplates from '../../../charts/templates';
 // Components
 import Card from '@/components/Card';
 import { options } from '@/pages/homePageData/data';
+import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
   const gameStats = JSON.parse(localStorage.getItem('gameStats'));
   const achievements = JSON.parse(localStorage.getItem('achievements'));
+  const [isAch, setIsAch] = useState(false);
+  const [pieChart, setPieChart] = useState(chartTemplates['pie']);
 
   let colChart = chartTemplates['column'];
-  let pieChart = chartTemplates['pie'];
 
-  let completedAch = 0;
-  if (achievements.playerstats.achievements) {
-    achievements.playerstats.achievements.forEach((item) => {
-      if (item.achieved) completedAch++;
-    });
-  }
+  // Column chart
+  useEffect(() => {
+    if (achievements.playerstats.achievements) {
+      setIsAch(true);
+      let completedAch = achievements.playerstats.achievements.filter((a) => a.achieved).length;
 
-  const totalAch = achievements.playerstats.achievements.length;
-  const uncompletedAch = totalAch - completedAch;
-  console.log(completedAch);
+      const totalAch = achievements.playerstats.achievements.length;
+      const uncompletedAch = totalAch - completedAch;
+      console.log(completedAch);
+
+      setPieChart((prev) => ({
+        ...prev,
+        options: {
+          ...prev.options,
+          labels: ['Achieved', 'Not achieved'],
+        },
+        series: [completedAch, uncompletedAch],
+      }));
+    }
+  }, []);
 
   const seriesData = gameStats.response.games.map((item) =>
     (item.playtime_forever / 60).toFixed(1)
@@ -34,42 +46,59 @@ export default function Dashboard() {
     ...colChart,
     options: {
       ...colChart.options,
+      chart: {
+        ...colChart.options.chart,
+        toolbar: {
+          show: false,
+        },
+      },
       colors: ['#CB3CFF', '#ffff'],
       xaxis: {
+        ...colChart.options.xaxis,
         categories: categories,
       },
     },
     series: [{ data: seriesData }],
   };
 
-  pieChart = {
-    ...pieChart,
-    options: {
-      ...pieChart.options,
-      labels: ['Achieved', 'Not achieved'],
-    },
-    series: [completedAch, uncompletedAch],
-  };
-
   return (
-    <div>
-      <Card>
-        <Chart
-          options={colChart.options}
-          series={colChart.series}
-          type="bar"
-          height={colChart.height}
-          width={colChart.width}
-        />
+    <div className="grid grid-cols-2 gap-4">
+      <section className="grid grid-rows-2 gap-4">
+        <Card>
+          <Chart
+            options={colChart.options}
+            series={colChart.series}
+            type="bar"
+            height="250"
+            width="500"
+          />
+        </Card>
+        <Card>
+          <Chart
+            options={colChart.options}
+            series={colChart.series}
+            type="bar"
+            height="250"
+            width="500"
+          />
+        </Card>
+      </section>
 
-        <Chart
-          options={pieChart.options}
-          series={pieChart.series}
-          type="pie"
-          height={pieChart.height}
-          width={pieChart.width}
-        />
-      </Card>
+      <section>
+        <Card>
+          {isAch ? (
+            <Chart
+              options={pieChart.options}
+              series={pieChart.series}
+              type="pie"
+              height="250"
+              width="500"
+            />
+          ) : (
+            <div>This game does not have any achievements</div>
+          )}
+        </Card>
+      </section>
     </div>
   );
 }
